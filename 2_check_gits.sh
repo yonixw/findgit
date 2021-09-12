e8596e953ccaa0a8f5d3fc3e1b1a98200de347b1
Yoni Water Man <yonixw@gmail.com>
2021-09-12 15:24:21 +0300

    cross solution for mac

--- 2_check_gits.sh
+++ 2_check_gits.sh
@@ -1,1 +1,2 @@
-cat all_gits.txt | xargs -n 1 -d '\n' bash _git_status_fast.sh
+#cat all_gits.txt | xargs -n 1 -d '\n' bash _git_status_fast.sh
+cat all_gits.txt | xargs -n 1 -I{} bash _git_status_fast.sh {}
